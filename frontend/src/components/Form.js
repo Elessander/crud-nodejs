65befedd8fd98b701100f963407c4fcf6c248c2b
Elessander <elessanderribovski775@hotmail.com>
2025-03-22 18:17:12 -0300

    finalização-do-backend

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,5 +1,7 @@
-import React, { useRef } from "react";
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
+import { toast } from "react-toastify";
 
 const FormContainer = styled.form`
     display: flex;
@@ -37,11 +39,66 @@ const Button = styled.button`
     height: 42px;
 `;
 
-const Form = ({onEdit}) => {
+const Form = ({ getProducts, onEdit, setOnEdit }) => {
     const ref = useRef();
 
+    useEffect(() => {
+        if (onEdit) {
+            const user = ref.current;
+
+            user.nome.value = onEdit.nome;
+            user.codigo.value = onEdit.codigo;
+            user.descricao.value = onEdit.descricao;
+            user.preco.value = onEdit.preco;
+        }
+    }, [onEdit]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user = ref.current;
+
+        if (
+            !user.nome.value || 
+            !user.codigo.value || 
+            !user.preco.value
+        ) {
+            return toast.warn("Preencha todos os campos!");
+        }
+
+    if (onEdit) {
+        await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+            nome: user.nome.value,
+            codigo: user.codigo.value,
+            descricao: user.descricao.value,
+            preco: user.preco.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+        await axios
+        .post("http://localhost:8800/", {
+            nome: user.nome.value,
+            codigo: user.codigo.value,
+            descricao: user.descricao.value,
+            preco: user.preco.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.codigo.value = "";
+    user.descricao.value = "";
+    user.preco.value = "";
+
+    setOnEdit(null);
+    getProducts();
+};
+
     return (
-        <FormContainer>
+        <FormContainer ref={ref} onSubmit={handleSubmit}>
             <InputArea>
                 <Label>Nome</Label>
                 <Input name="nome"/>
